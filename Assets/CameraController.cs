ab0cdaf81944e2d7eff6ffdc88d6c1dea01b341b
Ginkour <egsmiroff22@gmail.com>
2023-03-29 13:59:21 +0100

    aftopt cam fix

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,45 +6,26 @@ using UnityEngine.Rendering;
 
 public class CameraController : MonoBehaviour
 {
-    //assigned from outside class variables
-    public GameController gm = null;
-    public Transform playerTransform = null;
-
-    public static CameraController _camcont = null;
-    private Camera _camera = null;
+    [SerializeField] private Transform playerTransform = null;
     Vector3 standart_camera_pos;
-    private float standart_camera_size;
     // Start is called before the first frame update
     void Awake()
     {
-        gm = GameController._gameController;
-        _camera = GetComponent<Camera>();
-        _camcont = this;
-        standart_camera_pos = this.transform.position;
-        standart_camera_size = _camera.orthographicSize;
+        standart_camera_pos = transform.position;
     }
     private void Start()
     {
-        gm = GameController._gameController;
-        // assert if gm is null
-        Debug.Assert(gm != null, "Game controller is null", this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gm != null && playerTransform != null)
+        if (playerTransform != null)
         {
             Vector3 new_transform = playerTransform.position;
             if (new_transform.y < standart_camera_pos.y)
             {
-                _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, standart_camera_size, 0.001f);
                 new_transform.y = standart_camera_pos.y;
-            }
-            else
-            {
-                float new_camera_size = standart_camera_size + (new_transform.y - standart_camera_pos.y)/standart_camera_size;
-                _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, new_camera_size, 0.001f);
             }
             new_transform.z = standart_camera_pos.z;
             transform.position = new_transform;
