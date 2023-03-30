c2b44cb727d44514e5e1582896bd63b70d9d0bc9
zylo_dev <kkhaleelkk505@gmail.com>
2023-03-30 05:29:36 +0100

    Revert "Merge branch 'main' into background-handler"
    This reverts commit e2e2c441fcecc86b47fb2b839e74be2bdf1d8dbb, reversing
    changes made to 79853b4230a4b58177cc80e10c255cc3e970b412.

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Inventory _inventory;
     private Animator _animator;
-    public bool walk, holdTorch, ponchoOn, atacking, paused, right, healing, picking = false;
+    public bool walk, holdTorch, ponchoOn, atacking, paused, right = false;
     public GameObject gameOverGUI, playerGUI, pauseGUI;
 
 
@@ -14,7 +13,6 @@ public class PlayerController : MonoBehaviour
     SpriteRenderer spriteRenderer;
     public GameObject floorCheck;
 
-    [SerializeField] public int bandageHeals = 2;
     public int maxHealth = 10;
     public int playerHealth = 10;
 
@@ -31,19 +29,7 @@ public class PlayerController : MonoBehaviour
         Time.timeScale = 0;
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Bandage"))
-            if (_inventory.PickItem(Inventory.Items.Item.Bandage)) 
-                Object.Destroy(collision.GetComponent<GameObject>());
-        else if(collision.CompareTag("Torch"))
-            if(_inventory.PickItem(Inventory.Items.Item.Torch))
-                Object.Destroy(collision.GetComponent<GameObject>());
-        else if (collision.CompareTag("Poncho"))
-            if (_inventory.PickItem(Inventory.Items.Item.Poncho))
-                Object.Destroy(collision.GetComponent<GameObject>());
 
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -53,24 +39,18 @@ public class PlayerController : MonoBehaviour
         maxHealth = playerHealth;
         Debug.Assert(maxHealth != 0,"Health is 0 set it!");
         _animator = gameObject.GetComponent<Animator>();
-        _inventory = GetComponent<Inventory>();
     }
   
     // Update is called once per frame
     void Update()
     {
-        // heal button// do not need condition everything have done in Inventory update
-        healing = Input.GetKey(KeyCode.G);
-        // picking item key check
-        picking = Input.GetKeyUp(KeyCode.E);
-
         // debug damage player
         if (Input.GetKeyDown(KeyCode.K))
         {
             DamagePlayer();
         }
         // Torch key
-        else if (Input.GetKeyDown(KeyCode.T) && _inventory.Torch.Number >= 1)
+        else if (Input.GetKeyDown(KeyCode.T))
         {
             if (!holdTorch)
             {
@@ -82,7 +62,7 @@ public class PlayerController : MonoBehaviour
                 holdTorch = false;
         }
         // Poncho key
-        else if (Input.GetKeyDown(KeyCode.P) && _inventory.Poncho.Number >= 1)
+        else if (Input.GetKeyDown(KeyCode.P))
         {
             if (!ponchoOn)
             {
@@ -120,6 +100,8 @@ public class PlayerController : MonoBehaviour
                 pauseGUI.SetActive(false);
                 paused = false;
             }
+
+
         // Attack key
         if (Input.GetKeyDown(KeyCode.F))
         {
